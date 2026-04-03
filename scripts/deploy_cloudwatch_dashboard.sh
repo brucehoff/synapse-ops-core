56d8bdeebcf3d259bbb7b66336da078f4ad389da
bhoff <bruce.hoff@sagebase.org>
2026-04-02 20:24:28 -0700

    PLFM-9548

--- scripts/deploy_cloudwatch_dashboard.sh
+++ scripts/deploy_cloudwatch_dashboard.sh
@@ -28,7 +28,14 @@ SRC_PATH=${6}
 
 cd $SRC_PATH
 
+echo list all files
+
+ls -al
+
 # We run under the default AWS role
 python configuration.py $STACK $INSTANCE $REPO_BEANSTALK_NUMBER,$WORKERS_BEANSTALK_NUMBER,$PORTAL_BEANSTALK_NUMBER default
+
+
+npm install -g aws-cdk
 
 cdk deploy --profile default --context stack=$STACK --context stack_versions=$INSTANCE --context profile_name=default
