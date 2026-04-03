e49ff047226dc6c81e2b22084f3f58184b9aa615
bhoff <bruce.hoff@sagebase.org>
2026-04-03 10:29:08 -0700

    PLFM-9548

--- scripts/deploy_cloudwatch_dashboard.sh
+++ scripts/deploy_cloudwatch_dashboard.sh
@@ -23,8 +23,11 @@ WORKERS_BEANSTALK_NUMBER=${4}
 # e.g. 582-0
 PORTAL_BEANSTALK_NUMBER=${5}
 
+# True or False
+BEANSTALK_MODE=${6}
+
 # Folder containing source code
-SRC_PATH=${6}
+SRC_PATH=${7}
 
 cd $SRC_PATH
 
@@ -40,4 +43,4 @@ export CDK_DEFAULT_ACCOUNT=$(aws sts get-caller-identity --query Account --outpu
 export CDK_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
 echo "Account: $CDK_DEFAULT_ACCOUNT, Region: $CDK_DEFAULT_REGION"
 
-cdk deploy --context stack=$STACK --context stack_versions=$INSTANCE
+cdk deploy --context stack=$STACK --context stack_versions=$INSTANCE --context beanstalk_mode=$BEANSTALK_MODE
