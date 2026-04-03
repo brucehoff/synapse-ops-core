a49d4689191ba4332820072b0fb849b853b6b1d5
bhoff <bruce.hoff@sagebase.org>
2026-04-03 07:27:15 -0700

    PLFM-9548

--- scripts/deploy_cloudwatch_dashboard.sh
+++ scripts/deploy_cloudwatch_dashboard.sh
@@ -41,4 +41,4 @@ export CDK_DEFAULT_ACCOUNT=$(aws sts get-caller-identity --query Account --outpu
 export CDK_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
 echo "Account: $CDK_DEFAULT_ACCOUNT, Region: $CDK_DEFAULT_REGION"
 
-cdk deploy --profile default --context stack=$STACK --context stack_versions=$INSTANCE
+cdk deploy --context stack=$STACK --context stack_versions=$INSTANCE
