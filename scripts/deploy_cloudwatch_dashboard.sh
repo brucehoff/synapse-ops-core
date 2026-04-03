bcfe2f4c084fe2b6736c3b572bf502e0227b2677
bhoff <bruce.hoff@sagebase.org>
2026-04-02 19:54:24 -0700

    PLFM-9548

--- scripts/deploy_cloudwatch_dashboard.sh
+++ scripts/deploy_cloudwatch_dashboard.sh
@@ -11,16 +11,24 @@ STACK=${1}
 # instance, e.g. 583
 INSTANCE=${2}
 
-# repo, worker, portal beanstalk numbers
-# e.g. 582-0,582-0,582-0
-BEANSTALK_NUMBERS=${3}
+# repo beanstalk number
+# e.g. 582-0
+REPO_BEANSTALK_NUMBER=${3}
+
+# workers beanstalk number
+# e.g. 582-0
+WORKERS_BEANSTALK_NUMBER=${4}
+
+# portal beanstalk number
+# e.g. 582-0
+PORTAL_BEANSTALK_NUMBER=${5}
 
 # Folder containing source code
-SRC_PATH=${4}
+SRC_PATH=${6}
 
 cd $SRC_PATH
 
 # We run under the default AWS role
-python configuration.py $STACK $INSTANCXE $BEANSTALK_NUMBERS default
+python configuration.py $STACK $INSTANCXE $REPO_BEANSTALK_NUMBER,$WORKERS_BEANSTALK_NUMBER,$PORTAL_BEANSTALK_NUMBER default
 
 cdk deploy --profile default --context stack=$STACK --context stack_versions=$INSTANCE --context profile_name=default
