b7a6efcf3b88eb923c9e2e7f874341b9fe87e94a
bhoff <bruce.hoff@sagebase.org>
2026-04-17 16:39:12 -0700

    PLFM-9548

--- scripts/deploy_cloudwatch_dashboard.sh
+++ scripts/deploy_cloudwatch_dashboard.sh
@@ -8,40 +8,20 @@ set +x
 # dev or prod
 STACK=${1}
 
-# instance, e.g. 583
-INSTANCE=${2}
-
-# repo beanstalk number
-# e.g. 582-0
-REPO_BEANSTALK_NUMBER=${3}
-
-# workers beanstalk number
-# e.g. 582-0
-WORKERS_BEANSTALK_NUMBER=${4}
-
-# portal beanstalk number
-# e.g. 582-0
-PORTAL_BEANSTALK_NUMBER=${5}
-
-# True or False
-BEANSTALK_MODE=${6}
+# instances, e.g. 582,583,584,585
+INSTANCES=${2}
 
 # Folder containing source code
-SRC_PATH=${7}
+SRC_PATH=${3}
 
 cd $SRC_PATH
 
 pip install -r requirements.txt
 
-BEANSTALK_NUMBERS=$REPO_BEANSTALK_NUMBER,$WORKERS_BEANSTALK_NUMBER,$PORTAL_BEANSTALK_NUMBER
-python configuration.py $STACK $INSTANCE $BEANSTALK_NUMBERS
-
-
 npm install -g aws-cdk
 
-echo "Configuring CDK environment..."
 export CDK_DEFAULT_ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
 export CDK_DEFAULT_REGION=${AWS_DEFAULT_REGION:-us-east-1}
-echo "Account: $CDK_DEFAULT_ACCOUNT, Region: $CDK_DEFAULT_REGION"
 
-cdk deploy --context stack=$STACK --context stack_versions=$INSTANCE --context beanstalk_mode=$BEANSTALK_MODE
+cdk deploy --context stack=$STACK --context stack_versions=$INSTANCES --context beanstalk_mode=False
+
